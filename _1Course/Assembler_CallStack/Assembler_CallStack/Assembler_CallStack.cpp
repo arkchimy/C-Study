15edb5dbb7834527ca3cbddfb4738fab70effedd
DESKTOP-OB79R9E\정민혁 <arkchimy950323@gmail.com>
2025-07-05 13:33:49 +0900

    #CallStack의 쌓이는 구조 실험

---
+++ _1Course/Assembler_CallStack/Assembler_CallStack/Assembler_CallStack.cpp
@@ -0,0 +1,38 @@
+﻿// Assembler_CallStack.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <Windows.h>
+int Test(int a, int b)
+{
+    int x, y;
+    x = 0;
+    y = 0;
+
+    a = a * b;
+    x = a;
+
+    return x;
+}
+int Ebpcorruntion() 
+{
+    int x = 0;
+    int *pX = &x; 
+    Sleep(0);
+    
+    pX += 3;
+    *pX = x; //이전 ebp의 오염
+
+    return 0;
+}
+
+int main()
+{
+    int x = 0;
+   // printf("테스트하고싶다");
+    Test(10, 20);
+   Ebpcorruntion();
+    x = 100;
+
+    
+}
