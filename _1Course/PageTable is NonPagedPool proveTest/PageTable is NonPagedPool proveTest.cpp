7863203983dbf3f3729d2c8d0f1453c340cb87ab
DESKTOP-OB79R9E\정민혁 <arkchimy950323@gmail.com>
2025-07-09 10:44:11 +0900

    #NP풀은 커널메모리만 카운팅 된다

---
+++ _1Course/PageTable is NonPagedPool proveTest/PageTable is NonPagedPool proveTest.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <windows.h>
+
+struct stBigBuffer
+{
+    stBigBuffer()
+    {
+        _buffer = (char*)VirtualAlloc(nullptr, 4096 * 1024 *300 , MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+        if (_buffer == nullptr)
+            __debugbreak();
+        _limit = _buffer + 4096 * 1024 * 300;
+        _current = _buffer;
+    }
+    void Test()
+    {
+        _current += 100;
+        Sleep(10);
+        if (_current == _limit)
+        {
+            _current = _buffer;
+        }
+        _current[0] = 'a';
+    }
+    char *_buffer;
+    char *_limit;
+    char *_current = nullptr;
+};
+int main()
+{
+    stBigBuffer buffer;
+    while (1)
+    {
+        buffer.Test();
+    }
+
+}
