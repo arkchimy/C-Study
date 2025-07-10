585935c015413d42c167af1a2e9c5517f83b58f5
DESKTOP-OB79R9E\정민혁 <arkchimy950323@gmail.com>
2025-07-10 10:55:52 +0900

    #Large Page 에대해서 PageFault가 일어나는지에 대해 실험. - 첫 커밋에대해서도 PageFault가 발생하지않음을 확인

--- _1Course/Virtualloc_LargePageTest_PageFault/Virtualloc_LargePageTest_PageFault.cpp
+++ _1Course/Virtualloc_LargePageTest_PageFault/Virtualloc_LargePageTest_PageFault.cpp
@@ -1,68 +1,28 @@
-﻿
-//
-//#include <iostream>
-//#include <windows.h>
-//
-//int main()
-//{
-//    // Large Page 크기 확인
-//    SIZE_T largePageSize = GetLargePageMinimum();
-//    if (largePageSize == 0)
-//    {
-//        std::cerr << "Large pages not supported on this system." << std::endl;
-//        return 1;
-//    }
-//
-//    // 할당할 메모리 크기 (largePageSize의 배수여야 함)
-//    SIZE_T allocSize = largePageSize * 1; // 예: 2MB
-//
-//    // VirtualAlloc을 사용해 Large Page 메모리 할당
-//    void *lpMem = VirtualAlloc(
-//        NULL,
-//        allocSize,
-//        MEM_RESERVE | MEM_COMMIT | MEM_LARGE_PAGES,
-//        PAGE_READWRITE);
-//
-//    if (lpMem == NULL)
-//    {
-//        std::cerr << "VirtualAlloc failed with error: " << GetLastError() << std::endl;
-//        return 1;
-//    }
-//
-//    std::cout << "Large page memory allocated at: " << lpMem << std::endl;
-//
-//    // 메모리 해제
-//    VirtualFree(lpMem, 0, MEM_RELEASE);
-//
-//    return 0;
-//}
-
-
-// Virtualloc_LargePageTest_PageFault.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <Windows.h>
+﻿#include <Windows.h>
 #include <iostream>
 
-//#include "C:/Procamy/lib/Parser_lib/Parser_lib.h"
 #include "../../../Procademy/lib/Parser_lib/Parser_lib.h"
 
 #pragma comment(lib, "advapi32.lib")
-int g_mode;
+int g_mode = 1;
+int g_PageCnt = 1;
 
 char *_current;
 char *_buffer;
 DWORD _seh_filter(unsigned long code, LPEXCEPTION_POINTERS info)
 {
+    static int s_PageFaultCnt = 0;
+
     switch (code)
     {
     case EXCEPTION_ACCESS_VIOLATION:
         printf("Page Access Error\n");
-        _current = _buffer;
-        return EXCEPTION_CONTINUE_EXECUTION;
+        //_current = _buffer;
+        return EXCEPTION_EXECUTE_HANDLER;
     }
     printf("non define Error\n");
     return EXCEPTION_EXECUTE_HANDLER;
+    return EXCEPTION_CONTINUE_EXECUTION;
 }
 
 struct A
@@ -76,7 +36,7 @@ struct A
         if (!LookupPrivilegeValue(NULL, SE_LOCK_MEMORY_NAME, &luid))
         {
             printf("LookupPrivilegeValue error: %u\n", GetLastError());
-    
+
             return;
         }
 
@@ -93,57 +53,83 @@ struct A
                 NULL                      // ReturnLength: 필요 없으면 NULL
                 ))
             ;
-        
+
         size_t sizeds = GetLargePageMinimum();
-        if (g_mode)
+        if (g_mode == 1)
         {
-            _buffer = (char *)VirtualAlloc(nullptr, sizeds , MEM_RESERVE | MEM_COMMIT | MEM_LARGE_PAGES, PAGE_READWRITE);
+        realloc:
+            _buffer = (char *)VirtualAlloc(nullptr, sizeds * g_PageCnt, MEM_RESERVE | MEM_COMMIT | MEM_LARGE_PAGES, PAGE_READWRITE);
             if (GetLastError() != 0)
             {
                 printf("VirtualAlloc Error  %d \n", GetLastError());
-                Sleep(10000);
+                Sleep(5000);
+                goto realloc;
+            }
+            else
+            {
+                printf("LargePage VirtualAlloc Success  \n");
             }
         }
         else
         {
-            _buffer = (char *)VirtualAlloc(nullptr, sizeds , MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+            _buffer = (char *)VirtualAlloc(nullptr, sizeds * g_PageCnt, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+            if (_buffer != nullptr)
+                printf("VirtualAlloc Success  \n");
         }
         _current = _buffer;
     }
     ~A()
     {
+        VirtualFree(_buffer, 0, MEM_RELEASE);
     }
     void Access()
     {
         __try
         {
+
             *_current = 'a';
-            _current += 100;
+            _current += 4096;
+            printf("Access %p \n", _current);
         }
         __except (_seh_filter(GetExceptionCode(), GetExceptionInformation()))
         {
-            _current = _buffer;
+            printf("except _ execute \n");
         }
     }
-
 };
 int main()
 {
-    Parser parser;
-    parser.LoadFile(L"config.txt");
+    {
+        Parser parser;
+        parser.LoadFile(L"config.txt");
 
-    parser.GetValue(L"라지페이지On", g_mode);
+        parser.GetValue(L"LagePageTest", g_mode);
+        parser.GetValue(L"g_PageCnt", g_PageCnt);
+    }
 
-    A a;
+    A *a = nullptr;
+    a = new A();
 
+
+    size_t sizeds = GetLargePageMinimum();
+
+    printf("GetLargePageMinimum :   %zu \n", sizeds);
+
+    bool bStart = false;
     while (1)
     {
         if (GetAsyncKeyState(VK_ESCAPE))
         {
             return 0;
         }
-        a.Access();
-        Sleep(100);
+        if (GetAsyncKeyState('a') || GetAsyncKeyState('A'))
+        {
+   
+            bStart = true;
+        }
+        if (bStart)
+            a->Access();
+        Sleep(10);
     }
     return -1;
 }
