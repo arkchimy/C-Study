7f3ecc21eef1a61b7726c15dee76a6d94385837d
arkchimy950323 <arkchimy950323@gmail.com>
2025-07-11 15:52:44 +0900

    #TimeBeginPeriod 에 따른 성능 차이 Test - 1 로하니까 상승하는데  무슨 문제가 있는  Test인지 체크하기

---
+++ _3Course/_MultiThread_TimebeginPeriod_/_MultiThread_TimebeginPeriod_.cpp
@@ -0,0 +1,105 @@
+﻿// _MultiThread_TimebeginPeriod_.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <Windows.h>
+#include <conio.h>
+#include <iostream>
+#include <thread>
+
+#pragma comment(lib, "winmm.lib")
+
+#define THREADCNT 4
+long g_num;
+bool bOn = true;
+
+HANDLE hStartEvent;
+
+struct stTimePeriod
+{
+    stTimePeriod()
+    {
+        timeBeginPeriod(1);
+    }
+    ~stTimePeriod()
+    {
+        timeEndPeriod(1);
+    }
+};
+unsigned Add(void *arg)
+{
+    WaitForSingleObject(hStartEvent, INFINITE);
+    while (bOn)
+    {
+        InterlockedIncrement(&g_num);
+        Sleep(0);
+    }
+    return 0;
+}
+unsigned BlockThread(void *arg)
+{
+
+    WaitForSingleObject(hStartEvent, INFINITE);
+
+    while (bOn)
+    {
+        Sleep(0);
+    }
+    return 0;
+}
+
+unsigned MonitorThread(void *arg)
+{
+    WaitForSingleObject(hStartEvent, INFINITE);
+
+    DWORD currentTime = timeGetTime();
+    DWORD nextTime = currentTime + 1000;
+
+    while (bOn)
+    {
+
+        currentTime = timeGetTime();
+        if (nextTime <= currentTime)
+        {
+            printf("g_num %d \n", g_num);
+            InterlockedExchange(&g_num, 0);
+
+            nextTime += 1000;
+        }
+    }
+    return 0;
+}
+
+//stTimePeriod stPeriod;
+
+int main()
+{
+    for (int i = 0; i < THREADCNT; i++)
+        _beginthreadex(nullptr, 0, BlockThread, nullptr, 0, nullptr);
+    _beginthreadex(nullptr, 0, Add, nullptr, 0, nullptr);
+    _beginthreadex(nullptr, 0, MonitorThread, nullptr, 0, nullptr);
+
+    hStartEvent = CreateEvent(nullptr, true, 0, nullptr);
+    if (hStartEvent == nullptr)
+    {
+        __debugbreak();
+    }
+    while (1)
+    {
+        if (_kbhit())
+        {
+            char ch = _getch();
+
+            if (ch == 'a' || ch == 'A')
+            {
+                SetEvent(hStartEvent);
+            }
+            if (ch == 's' || ch == 'S')
+            {
+                bOn = FALSE;
+                return 0;
+            }
+        }
+    }
+
+
+}
