d3107d9ce06b7465bb07d28296236ef68736b98d
DESKTOP-OB79R9E\정민혁 <arkchimy950323@gmail.com>
2025-08-04 16:20:43 +0900

    #APC는 NP풀이다. => Thread 를 생성할때 alertable 상태로 진입해서 초기화 하는것으로 보임 => 생성하고 APC 큐에 넣는 작업을 바로 실행하면 apc가 실행됨. => Waitfor---EX 의 경우 APC를 먼저 실행한다. => NP 풀은  메모리 비례한 75%까지 되는것 같다

--- _2Course/SerializeBuffer_Project/CMessage.h
+++ _2Course/SerializeBuffer_Project/CMessage.h
@@ -7,14 +7,46 @@ concept Fundamental = std::is_fundamental_v<T>;
 
 struct CMessage
 {
+    enum class en_ErrorType
+    {
+        hasNotData, // 빼려고하는데 데이터가 없다.
+        notenouthSpace,
+        MAX,
+    };
+    enum class en_BufferSize
+    {
+        bufferSize = 1000,
+        MaxSize = 2000,
+    };
     CMessage()
     {
-        _rear = _buffer;
-        _front = _buffer;
+        static volatile long long oneChk = 0;
+        if (oneChk == 0)
+        {
+            if (InterlockedExchange64(&oneChk, 1) == 0)
+            {
+                s_BufferHeap = HeapCreate(HEAP_GENERATE_EXCEPTIONS, 0, 0);
+                if (s_BufferHeap == nullptr)
+                {
+                    printf("s_BufferHeap Create Failed \n");
+                    __debugbreak();
+                }
+            }
+        }
 
-        _begin = _buffer;
-        _end = _buffer;
-        ZeroMemory(_buffer, sizeof(_buffer));
+        _size = (DWORD)en_BufferSize::bufferSize;
+        _begin = (char *)HeapAlloc(s_BufferHeap, 0, _size);
+        _end = _begin + _size;
+
+        if (_begin == nullptr)
+            __debugbreak();
+
+
+        _rear = _begin;
+        _front = _begin;
+
+        ZeroMemory(_begin, _size);
+
     }
     void *operator new(size_t size)
     {
@@ -33,25 +65,34 @@ struct CMessage
         }
         return HeapAlloc(s_CMessageHeap, HEAP_ZERO_MEMORY | HEAP_GENERATE_EXCEPTIONS, sizeof(CMessage));
     }
+
     template <Fundamental T>
     CMessage &operator<<(const T &data)
     {
+        if (_end < _rear + sizeof(data))
+            throw en_ErrorType::notenouthSpace;
+
         memcpy(_rear, &data, sizeof(data));
         _rear += sizeof(data);
+
         return *this;
     }
+
     template <Fundamental T>
     CMessage &operator>>(T &data)
     {
         memcpy(&data, _front, sizeof(data));
         _front += sizeof(data);
 
+        if (_front > _rear)
+            throw en_ErrorType::hasNotData;
         return *this;
     }
+    DWORD _size;
 
-    char _buffer[1000];
     char *_begin, *_end;
     char *_rear, *_front;
 
     static HANDLE s_CMessageHeap;
+    static HANDLE s_BufferHeap;
 };
