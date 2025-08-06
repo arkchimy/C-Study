7db387784397acdf23bee0c5304ae3f1a999a9b5
arkchimy950323 <arkchimy950323@gmail.com>
2025-08-06 19:01:39 +0900

    #Async_sync_Completion Order Test

---
+++ _3Course/Async_Sync_Complet_OrderTest/Async_Sync_Complet_OrderTest.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <thread>
+
+
+#pragma comment(lib, "winmm")
+#pragma comment(lib, "ws2_32")
+
+SOCKET listen_sock;
+
+unsigned AcceptThread(void* arg)
+{
+    SOCKET client_sock;
+    SOCKADDR_IN addr;
+    int addrlen = sizeof(addr);
+
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(6000);
+
+    client_sock = accept(listen_sock, (sockaddr *)&addr, &addrlen);
+    if (client_sock == INVALID_SOCKET)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main()
+{
+
+    WSAData wsa;
+    HANDLE hPort,hAcceptThread;
+    DWORD wsaStartRetval;
+
+    wsaStartRetval = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    hPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, NULL, 0);
+
+    hAcceptThread = (HANDLE)_beginthreadex(nullptr, 0, AcceptThread, nullptr, 0, nullptr);
+
+
+    
+    WSACleanup();
+
+}
