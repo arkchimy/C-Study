c24c71a3dc1eb01e3ac5a4b5f36fb47a8a457bf0
DESKTOP-OB79R9E\정민혁 <arkchimy950323@gmail.com>
2025-11-12 14:27:58 +0900

    #하트비트에 대한 고민

--- _3Course/EchoServer_ContentsThread/CTestServer.h
+++ _3Course/EchoServer_ContentsThread/CTestServer.h
@@ -2,13 +2,21 @@
 #include "../lib/CNetwork_lib/CNetwork_lib.h"
 #include <stack>
 #include <unordered_map>
+    #include <set>
+
+#define dfRANGE_MOVE_TOP 0
+#define dfRANGE_MOVE_LEFT 0
+#define dfRANGE_MOVE_RIGHT 6400
+#define dfRANGE_MOVE_BOTTOM 6400
+// #define dfRANGE_MOVE_RIGHT 640
+// #define dfRANGE_MOVE_BOTTOM 640
+#define dfSECTOR_Size 128
 
 struct CPlayer
 {
-    ull m_sessionID;
-
     DWORD m_Timer;
     INT64 m_AccountNo;
+
     WCHAR m_ID[20];
     WCHAR m_Nickname[20];
     char m_SessionKey[64];
@@ -45,19 +53,47 @@ class CTestServer : public CLanServer
     HANDLE hContentsThread = 0;
     HANDLE hMonitorThread = 0 ;
 
-    // TODO: 특정 인원이상 안늘어나게 조치.
-    CObjectPool_UnSafeMT<CPlayer> player_pool; 
-
-    // Account   Key , Player접근.
-    std::unordered_map<ull, CPlayer*> AccountNo_hash; // 중복 접속을 제거하는 용도
-    // SessionID Key , Player접근.
-    std::unordered_map<ull, CPlayer*> SessionID_hash; // 중복 접속을 제거하는 용도
-
 
     HANDLE m_ContentsEvent = INVALID_HANDLE_VALUE;
     HANDLE m_ServerOffEvent = INVALID_HANDLE_VALUE;
+
     int m_maxSessions = 0;   
     int m_maxPlayers = 7000;   
     LONG64 m_TotalPlayers = 0; // 현재 Player의 Cnt
     int m_State_Session = 0; // 아직 Player가 되지못한 Session의 Cnt
+
+
+    /*
+        
+        Player에서 SessionID를 들고있는 현상을 고치기 위해서는.
+        ChatingServer 를 기준으로 CPlayer를 갖고 남의 Player에 접근할 일이 없다.
+
+        Sector 자체의 템플릿을 SessionID로 둠으로써 SendPacket에 빠른 접근이 가능하도록하자.
+        inline static std::set<ull>   
+    */
+    // TODO: 특정 인원이상 안늘어나게 조치.
+    CObjectPool_UnSafeMT<CPlayer> player_pool;
+
+    // Account   Key , Player접근.
+    std::unordered_map<ull, CPlayer *> AccountNo_hash; // 중복 접속을 제거하는 용도
+
+    // SessionID Key , Player접근.
+    std::unordered_map<ull, CPlayer *> SessionID_hash; // 중복 접속을 제거하는 용도
+
+    // SessionID 를 삽입 
+    inline static std::set<ull>
+        g_Sector[dfRANGE_MOVE_BOTTOM / dfSECTOR_Size]
+                [dfRANGE_MOVE_BOTTOM / dfSECTOR_Size];
+
+    /*
+      하트비트 처리 방법. 
+      일단 LoginPacket을 보내지않는 Session에 대해서 처리 방법이 필요함.
+      
+      OnAccept에서 PQCS를하고 그 메세지로 인증되지않은 Player를 생성.
+      ContentsThread는 LoginPacket을 받는다면 인증되지않는 Player를 SessionID로 탐색 후 제거에 성공한다면, Player로 승격,
+      
+      Contetns는 인증되지않는 Player를 주기적으로 검사한다.
+      
+    */
+
 };
