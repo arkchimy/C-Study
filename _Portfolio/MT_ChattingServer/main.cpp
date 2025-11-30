46423d218d0267e3fba302f706acdad8baf5c62d
DESKTOP-99KN4IA\정민혁 <arkchimy950323@gmail.com>
2025-11-30 15:50:33 +0900

    #NetWorkLib - 라이브러리에 필요하거나 유틸성있는 기능들, Parser , Profiler 기능을  포함하여 하나로 둠. => 이후 라이브러리를 이용한 작성이 편해짐. - 내부에 IDL_Complier exe를 둠.  이 기능을 이용해 stub 과 proxy를 작성 # IDL_Complier - 이 기능은  현재 Networklib에서 편하게 설정되어있음. - Proxy와 stub이 server를 포함해야하는 구조인데. 이 경로를 Networklib의 내부 RPC폴더에 넣어두고 작동.

---
+++ _Portfolio/MT_ChattingServer/main.cpp
@@ -0,0 +1,111 @@
+#include <conio.h>
+#include <thread>
+
+#include "CMTChattingServer.h"
+#include "CrushDump_lib/CrushDump_lib.h"
+
+int main()
+{
+    CDump::SetHandlerDump();
+
+    st_WSAData wsa;
+
+    wchar_t bindAddr[16];
+    short bindPort;
+
+    int iZeroCopy;
+    int iEnCording;
+    int WorkerThreadCnt ,ContentsThreadCnt;
+    int reduceThreadCount;
+    int NoDelay;
+    int maxSessions;
+
+    LINGER linger;
+    int iRingBufferSize;
+    int ContentsRingBufferSize;
+
+    HRESULT hr;
+
+    {
+        Parser parser;
+
+        if (parser.LoadFile(L"Config.txt") == false)
+            CSystemLog::GetInstance()->Log(L"ParserError.txt", en_LOG_LEVEL::ERROR_Mode, L"LoadFileError %d", GetLastError());
+        parser.GetValue(L"ServerAddr", bindAddr, 16);
+        parser.GetValue(L"ServerPort", bindPort);
+
+        parser.GetValue(L"LingerOn", linger.l_onoff);
+        parser.GetValue(L"ZeroCopy", iZeroCopy);
+
+        parser.GetValue(L"WorkerThreadCnt", WorkerThreadCnt);
+        parser.GetValue(L"ReduceThreadCount", reduceThreadCount);
+        parser.GetValue(L"NoDelay", NoDelay);
+        parser.GetValue(L"MaxSessions", maxSessions);
+
+        parser.GetValue(L"ContentsThreadCnt", ContentsThreadCnt);
+        parser.GetValue(L"RingBufferSize", iRingBufferSize);
+        parser.GetValue(L"ContentsRingBufferSize", ContentsRingBufferSize);
+        parser.GetValue(L"EnCording", iEnCording);
+
+
+        CRingBuffer::s_BufferSize = iRingBufferSize;
+    }
+    wchar_t buffer[100];
+
+    StringCchPrintfW(buffer, 100, L"Profiler_%hs.txt", __DATE__);
+
+    {
+        CTestServer::s_ContentsQsize = ContentsRingBufferSize;
+        CTestServer *ChattingServer = new CTestServer(ContentsThreadCnt, iEnCording);
+
+        ChattingServer->Start(bindAddr, bindPort, iZeroCopy, WorkerThreadCnt, reduceThreadCount, NoDelay, maxSessions);
+
+        CSystemLog::GetInstance()->SetDirectory(L"SystemLog");
+        CSystemLog::GetInstance()->SetLogLevel(en_LOG_LEVEL::ERROR_Mode);
+        while (1)
+        {
+            if (GetAsyncKeyState(VK_ESCAPE))
+            {
+                CSystemLog::GetInstance()->Log(L"SystemLog.txt", en_LOG_LEVEL::SYSTEM_Mode, L"Server Stop");
+                CSystemLog::GetInstance()->Log(L"Socket_Error.txt", en_LOG_LEVEL::SYSTEM_Mode, L"Server Stop");
+                ChattingServer->Stop();
+                SetEvent(ChattingServer->m_ServerOffEvent);
+                break;
+            }
+            if (_kbhit())
+            {
+                char ch = _getch();
+                if (ch == 'A' || ch == 'a')
+                {
+                    CSystemLog::GetInstance()->SaveAsLog();
+                    Profiler::SaveAsLog(buffer);
+                }
+                if (ch == 'D' || ch == 'd')
+                {
+                    Profiler::Reset();
+                }
+                if (ch == '1')
+                {
+
+                    CSystemLog::GetInstance()->SetLogLevel(en_LOG_LEVEL::ERROR_Mode);
+                    printf("ERROR_Mode\n");
+                }
+                if (ch == '2')
+                {
+
+                    CSystemLog::GetInstance()->SetLogLevel(en_LOG_LEVEL::DEBUG_TargetMode);
+                    printf("DEBUG_TargetMode\n");
+                }
+                if (ch == '3')
+                {
+
+                    CSystemLog::GetInstance()->SetLogLevel(en_LOG_LEVEL::DEBUG_Mode);
+                    printf("DEBUG_Mode\n");
+                }
+            }
+        }
+
+        Sleep(10000);
+    }
+   
+}
