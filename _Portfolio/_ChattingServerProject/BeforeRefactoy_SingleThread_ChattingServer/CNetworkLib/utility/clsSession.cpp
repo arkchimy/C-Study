d858179466b5b187e668bf284abe90765321c49b
DESKTOP-99KN4IA\정민혁 <arkchimy950323@gmail.com>
2025-12-05 17:54:45 +0900

    #개선전 SingleChatting - 알기쉽게 프로파일링 이름 바꿈.

---
+++ _Portfolio/_ChattingServerProject/BeforeRefactoy_SingleThread_ChattingServer/CNetworkLib/utility/clsSession.cpp
@@ -0,0 +1,35 @@
+#include "clsSession.h"
+
+#include "../utility/CSystemLog/CSystemLog.h"
+#include "../utility/SerializeBuffer_exception/SerializeBuffer_exception.h"
+#include "../utility/CTlsObjectPool/CTlsObjectPool.h"
+
+extern template PVOID stTlsObjectPool<CMessage>::Alloc();       // 암시적 인스턴스화 금지
+extern template void stTlsObjectPool<CMessage>::Release(PVOID); // 암시적 인스턴스화 금지
+
+clsSession::clsSession(SOCKET sock)
+    : m_sock(sock)
+{
+    m_blive = 1;
+}
+
+clsSession::~clsSession()
+{
+    closesocket(m_sock);
+    __debugbreak();
+}
+
+void clsSession::Release()
+{
+    CMessage *msg;
+    while (m_sendBuffer.Pop(msg))
+    {
+        stTlsObjectPool<CMessage>::Release(msg);
+    }
+
+    {
+        ZeroMemory(&m_recvOverlapped, sizeof(OVERLAPPED));
+        ZeroMemory(&m_sendOverlapped, sizeof(OVERLAPPED));
+    }
+    m_recvBuffer.ClearBuffer();
+}
