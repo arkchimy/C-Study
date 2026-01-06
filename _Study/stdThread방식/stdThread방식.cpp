11ab1604bdc85aa5e0770396a3f112cad9262d83
DESKTOP-OB79R9E\정민혁 <arkchimy950323@gmail.com>
2026-01-06 13:49:17 +0900

    #std::Thread 방식으로 예제구현 성공

--- _Study/stdThread방식/stdThread방식.cpp
+++ _Study/stdThread방식/stdThread방식.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <Windows.h>
-#include <process.h>
+#include <thread>
 
 class CLanServer
 {
@@ -16,21 +16,37 @@ class CLanServer
 // 그 기본 함수는 매개변수로 받고, 매개변수로 받은 함수를 해당 instance 를 이용해 멤버함수를 호출한다.
 
 template <typename T>
-HANDLE MyCreateThread(void (T::*pmf)(void), T *instance = this) // 이거 되나?
-{
-    std::pair<void (T::*)(void), T *> arg({pmf, instance});
-    return (HANDLE)_beginthreadex(nullptr, 0, &StartRoutine<T>, arg, 0, nullptr);
-}
-
-template <typename T>
 unsigned StartRoutine(void* arg)
 {
-    std::pair<void (T::*)(void), T *> data = static_cast<std::pair<void (T::*)(void), T *>>(arg);
-    T *instance = data->second;
-    (instance->*)data->first();
+    std::pair<void (T::*)(void), T *>* data = static_cast<std::pair<void (T::*)(void), T *>*>(arg);
+    (data->second->*data->first)(); //  (괄호 주의)
+    delete data;
+    return 0;
 }
+
+
+
+struct MyThread
+{
+
+    template <typename T>
+    MyThread(void (T::*pmf)(void), T* instance)
+    {
+        // 소멸자에서 delete 
+        std::pair<void (T::*)(void), T *> *arg = new std::pair<void (T::*)(void), T *>({pmf,instance}); 
+        _hThread = (HANDLE)_beginthreadex(nullptr, 0, &StartRoutine<T>, arg, 0, nullptr);
+    }
+    ~MyThread()
+    {
+        CloseHandle(_hThread);
+    }
+    HANDLE _hThread;
+};
 
 int main()
 {
-
+    CLanServer server;
+    CLanServer* pServer = &server;
+    MyThread th2(&CLanServer::WorkerThread, pServer);
+    WaitForSingleObject(th2._hThread, INFINITE);
 }
