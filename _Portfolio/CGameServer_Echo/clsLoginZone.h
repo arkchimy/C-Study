639d2bcd9721c7860188533c700ac8696e75337e
arkchimy950323 <arkchimy950323@gmail.com>
2026-01-18 17:22:49 +0900

    #GameServer - Lock 을 안잡는 방향으로 ㄱㄱ

--- _Portfolio/CGameServer_Echo/clsLoginZone.h
+++ _Portfolio/CGameServer_Echo/clsLoginZone.h
@@ -3,6 +3,16 @@
 #include <unordered_map>
 #include "enZoneType.h"
 
+/*
+* 
+*     // 해당 Hash 는 다른 Zone에서도 접근함.
+    std::unordered_map<ull, stPlayer *> SessionID_hash;
+    // 중복 로그인 주의
+
+    std::unordered_map<INT64, stPlayer *> Account_hash;
+
+    이 부분을 Server 함수를 통해 전달하여 Lock을 걸고 접근하는 방식을 제거.
+*/
 class clsLoginZone : public IZone
 {
     enum enMsgType : std::uint8_t
